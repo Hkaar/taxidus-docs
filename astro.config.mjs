269af5ced305686fa46cb6e96bc1b622ee04ce85
Hkaar <HkaarDev@gmail.com>
2024-12-31 21:50:25 +0800

    Updated redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,4 +13,17 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
+
+  // To change these change clear the cached images and site data in the browser first
+  // to do this in chrome or firefox just press (CTRL+SHIFT+DELETE)
+  redirects: {
+    "/docs": {
+      destination: "/docs/intro",
+      status: 303,
+    },
+    "/guides": {
+      destination: "/guides/intro",
+      status: 303,
+    },
+  },
 });
