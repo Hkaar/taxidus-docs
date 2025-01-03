07d6f82b6260fe744ab21af63a0bf695769c2e80
Hkaar <HkaarDev@gmail.com>
2025-01-03 11:22:57 +0800

    Updated content config

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,15 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
+import { X } from "lucide-react";
 
 const docs = defineCollection({
     loader: glob({ pattern: "**/*.md", base: "./src/content/docs" }),
     schema: z.object({
         title: z.string(),
         slug: z.string(),
-        date: z.coerce.date(),
         tags: z.array(z.string()),
+        group: z.string().optional(),
+        lastModified: z.coerce.date(),
     }),
 });
 
@@ -16,8 +18,9 @@ const guides = defineCollection({
     schema: z.object({
         title: z.string(),
         slug: z.string(),
-        date: z.coerce.date(),
         tags: z.array(z.string()),
+        group: z.string().optional(),
+        lastModified: z.coerce.date(),
     }),
 });
 
