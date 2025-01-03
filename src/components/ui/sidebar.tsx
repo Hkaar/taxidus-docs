b9a5370a1b057e22b5a3b0af70aca93b22446c47
Hkaar <HkaarDev@gmail.com>
2025-01-03 11:23:51 +0800

    Added sidebar component

---
+++ src/components/ui/sidebar.tsx
@@ -0,0 +1,183 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Button, buttonVariants, type ButtonProps } from "@/components/ui/button";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./accordion";
+
+const ToggleSideBar = (elementID: string, forceClose?: boolean) => {
+  const sidebar = document.getElementById(elementID);
+  const overlay = document.getElementById(`${elementID}-overlay`);
+
+  if (!sidebar) {
+    console.error(`Sidebar with id ${elementID} does not exist!`);
+    return;
+  }
+
+  if (forceClose || !sidebar.classList.contains("-translate-x-full")) {
+    sidebar.classList.add("-translate-x-full");
+    overlay?.classList.add("hidden");
+  } else {
+    sidebar.classList.remove("-translate-x-full");
+    overlay?.classList.remove("hidden");
+  }
+}
+
+export interface SideBarOverlayProps extends Omit<React.ComponentProps<"div">, "className" | "children"> {
+  sideBarId: string;
+  className?: string;
+}
+
+const SideBarOverlay = ({ sideBarId, className }: SideBarOverlayProps) => {
+  return (
+    <div
+      id={`${sideBarId}-overlay`}
+      className={cn(
+        "fixed inset-0 bg-black/50 z-30 hidden",
+        "xl:hidden",
+        className
+      )}
+      onClick={() => ToggleSideBar(sideBarId, true)}
+    />
+  );
+};
+
+export interface SideBarContainerProps extends Omit<React.ComponentProps<"div">, "className" | "children"> {
+  id: string;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const SideBarContainer = React.forwardRef<HTMLDivElement, SideBarContainerProps>(
+  ({ id, className, children, ...props }, ref) => {
+    return (
+      <>
+        <SideBarOverlay sideBarId={id} />
+        <div className={cn("fixed top-0 flex flex-col w-screen lg:w-full -translate-x-full overflow-y-auto max-w-full min-h-screen z-40 transition-all duration-300 ease-in-out md:max-w-96 xl:relative xl:z-0 xl:h-auto xl:translate-x-0 bg-gray-100 dark:bg-neutral-900", className)} ref={ref} id={id} {...props}>
+          {children}
+        </div>
+      </>
+    )
+  }
+)
+
+export interface SideBarHeaderProps extends Omit<React.ComponentProps<"div">, "className" | "children"> {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const SideBarHeader = React.forwardRef<HTMLDivElement, SideBarHeaderProps>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div className={cn("px-6 py-5 bg-gray-200 dark:bg-neutral-800", className)} ref={ref} {...props}>
+        {children}
+      </div>
+    )
+  }
+)
+
+export interface SideBarContentProps extends Omit<React.ComponentProps<"div">, "className" | "children"> {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const SideBarContent = React.forwardRef<HTMLDivElement, SideBarContentProps>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div className={cn("flex-1 px-6 py-3", className)} ref={ref} {...props}>
+        {children}
+      </div>
+    )
+  }
+)
+
+export interface SideBarFooterProps extends Omit<React.ComponentProps<"div">, "className" | "children"> {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const SideBarFooter = React.forwardRef<HTMLDivElement, SideBarFooterProps>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div className={cn("px-6 py-5 bg-gray-200 dark:bg-neutral-800", className)} ref={ref} {...props}>
+        {children}
+      </div>
+    )
+  }
+)
+
+interface SideBarToggleProps extends ButtonProps {
+  sideBarId: string;
+  variant?: "outline" | "ghost";
+}
+
+const SideBarToggle = ({ className, children, variant, sideBarId }: SideBarToggleProps) => {
+  const handleToggle = () => ToggleSideBar(sideBarId);
+
+  return (
+    <Button className={className} variant={variant || "outline"} size={"icon"} onClick={handleToggle}>
+      {children}
+    </Button>
+  )
+}
+
+interface SideBarItemProps {
+  title: string;
+  href: string;
+  icon?: React.ReactNode;
+  className?: string;
+}
+
+const SideBarItem = ({ title, href, icon, className }: SideBarItemProps) => {
+  return (
+    <a href={href} className={cn("flex items-center gap-2", buttonVariants({ variant: "link" }), className)}>
+      {icon} {title}
+    </a>
+  );
+};
+
+export type SideBarMenuItem = {
+  title: string;
+  href: string;
+}
+
+interface SideBarMenuProps {
+  title: string;
+  icon?: React.ReactNode;
+  className?: string;
+  items?: SideBarMenuItem[];
+}
+
+const SideBarMenu = ({ title, icon, items, className }: SideBarMenuProps) => {
+  return (
+    <Accordion type="single" className="w-full" collapsible>
+      <AccordionItem value="item-1" className={cn("border-0", className)}>
+        <AccordionTrigger>
+          <span className="flex items-center gap-3 capitalize">
+            {icon}
+            {title}
+          </span>
+        </AccordionTrigger>
+        <AccordionContent>
+          <ul className="border-l-2 border-gray-200 dark:border-neutral-800">
+            {items?.map((item, index) => (
+              <li key={`nested-side-bar-item-${index}`}>
+                <SideBarItem title={item.title} href={item.href} />
+              </li>
+            ))}
+          </ul>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  )
+};
+
+export {
+  SideBarContainer,
+  SideBarHeader,
+  SideBarContent,
+  SideBarFooter,
+  SideBarToggle,
+  SideBarItem,
+  SideBarMenu,
+  ToggleSideBar,
+}
