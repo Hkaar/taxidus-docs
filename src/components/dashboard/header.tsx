823d250081b474f67892122e2663814e673bb4aa
Hkaar <HkaarDev@gmail.com>
2025-01-16 16:12:32 +0800

    Updated header components

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -3,22 +3,10 @@ import ThemeToggle from "@/components/ThemeToggle";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Github from "@/components/icons/Github";
 import { cn } from "@/lib/utils";
-import { Home, LogOut, Menu, PanelLeft, Settings, User2 } from "lucide-react";
-import { useEffect, useState } from "react";
+import { Menu, PanelLeft } from "lucide-react";
+import { useState } from "react";
 import { SideBarToggle } from "@/components/ui/sidebar";
-import Authorized from "@/components/auth/Authorized";
-import Guest from "@/components/auth/Guest";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
-import defaultAvatar from "@/assets/profile.png";
-import { logout } from "@/utils/auth";
+import UserMenu from "../UserMenu";
 
 interface HeaderProps {
   path?: string;
@@ -88,65 +76,7 @@ const DashboardHeader = ({ className, path }: HeaderProps) => {
           </div>
 
           <div className="flex items-center gap-1">
-            <Authorized invisible>
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <img
-                    src={defaultAvatar.src}
-                    alt="Profile"
-                    className="rounded-full aspect-square size-8"
-                  />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>
-                    <a href="/" className="flex items-center gap-2">
-                      <User2 strokeWidth={1.5} />
-                      Profile
-                    </a>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <a href="/" className="flex items-center gap-2">
-                      <Home strokeWidth={1.5} />
-                      Home
-                    </a>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <a href="/" className="flex items-center gap-2">
-                      <Settings strokeWidth={1.5} />
-                      Settings
-                    </a>
-                  </DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem
-                    onClick={async () => {
-                      await logout();
-                      globalThis.window.location.reload();
-                    }}
-                  >
-                    <LogOut strokeWidth={1.5} />
-                    Logout
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </Authorized>
-
-            <Guest invisible>
-              <a
-                href="/sign-up"
-                className={buttonVariants({ variant: "default" })}
-              >
-                Sign up
-              </a>
-
-              <a
-                href="/login"
-                className={buttonVariants({ variant: "outline" })}
-              >
-                Login
-              </a>
-            </Guest>
+            <UserMenu />
           </div>
         </div>
       </div>
