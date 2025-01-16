32e805910a6c378addde4ce2da3a40146bc92268
Hkaar <HkaarDev@gmail.com>
2025-01-16 16:14:36 +0800

    Updated auth utils

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,25 +1,43 @@
 import axios from 'axios';
 import { toast } from 'sonner';
+import { fetchAPI } from './network';
 
-export async function logout() {
-    const token = localStorage.getItem('session_token');
-    const apiURL = import.meta.env.PUBLIC_API_URL;
+export async function getUserDetails(key: string) {
+    const raw = localStorage.getItem("user-data");
 
-    if (!token) {
-        return false;
+    if (!raw) {
+        const result = await fetchAPI<UserData>({ url: "/user", method: "GET", authorized: true });
+
+        if (result.error) {
+            toast.error(result.error);
+        }
+
+        localStorage.setItem("user-data", JSON.stringify(result.data));
+
+        if (key in result.data) {
+            return result.data[key];
+        }
     }
 
-    try {
-        await axios.post(`${apiURL}/logout`, {}, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
+    const userData: UserData = JSON.parse(raw as string);
+
+    if (key in userData) {
+        return userData[key];
+    }
+
+    return null;
+}
+
+export async function logout() {
+    const result = await fetchAPI<{ message: string }>({ url: "/logout", method: "POST", authorized: true });
+
+    if (result.success) {
         localStorage.removeItem('session_token');
 
         toast.success("Successfully logged out!");
         return true;
-    } catch (error) {
-        return false;
     }
+
+    toast.error("Failed to logout");
+    return false;
 }
